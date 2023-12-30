b9bc34e759a8e98a52473682aa20c3c58082ac78
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2023-12-30 01:03:05 -0500

    error handling done

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,7 +6,8 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
     const [searchKeyword, setSearchKeyword] = useState("");
     const [results, setResults] = useState([]);
-    
+    const [error, setError] = useState("")
+
     const searchApi = async () => {
         console.log("searching now")
         try {
@@ -14,15 +15,16 @@ const SearchScreen = () => {
                 params: {
                     limit: 50,
                     term: searchKeyword,
-                    location: 'san jose'
+                    location: 'new jersey'
                 }
             })
             console.log(response.data)
             setResults(response.data.businesses)
         } catch (er) {
             console.log(er)
+            setError("Something went wrong, Please try again!")
         }
-    }
+    } 
    
     return <View style={styles.parentStyle}>
         <SearchBar
@@ -35,7 +37,7 @@ const SearchScreen = () => {
                 console.log("submitting now!!")
                 searchApi()
             }}/>
-        <Text>{searchKeyword}</Text>
+        {error ? <Text>{ error }</Text> : null}
         <Text>Search Results: { results.length }</Text>
     </View>
 }
